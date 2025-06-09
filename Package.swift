26dbf63d60de1544bdb53ffd16ab5656358cd53f
noahcheng <noahcheng@tencent.com>
2025-06-09 20:45:10 +0800

    TUICore test SPM 15

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Tencent-RTC/Chat_SDK_SwiftPM", from: "8.3.6498"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,17 +23,12 @@ let package = Package(
             url: "https://liteav.sdk.qcloud.com/app/tuikit/download/customer/zhenxin/8.5/TUICore.xcframework.zip",
             checksum: "5bd2ca23dbfc0743013a5fcc733068c086fa61e1f888e1de0bcb0728836ac20e"
         ),
-        .binaryTarget(
-            name: "SDWebImage",
-            url: "https://github.com/SDWebImage/SDWebImage/releases/download/5.21.0/SDWebImage-dynamic.xcframework.zip",
-            checksum: "e034ea04f5e86866bc3081d009941bd5b2a2ed705b3a06336656484514116638"
-        ),
         .target(
             name: "TUICore_SwiftPM",
             dependencies: [
-                "SDWebImage",
                 "TUICore",
                 .product(name: "Chat_SDK_SwiftPM", package: "Chat_SDK_SwiftPM"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
             ]),
         .testTarget(
             name: "TUICore_SwiftPMTests",
